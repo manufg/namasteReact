f3100568be6b292b7a6ac7bac0a274fcc9207dc3
Manu FG <manufg1904@gmail.com>
2024-11-05 09:09:15 +0530

    Why jsx exists

--- app.js
+++ app.js
@@ -8,14 +8,18 @@
      </div>
 */
 
-const nested = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
+const nested = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
     React.createElement("h1", { id: "heading" }, "h1 tag"),
     React.createElement("h2", { id: "heading1" }, "h2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "h1 tag"),
+    React.createElement("h2", { id: "heading1" }, "h2 tag"),
+  ]),
+]);
+
+// this is vry tideous thats why JSX exists.
 
 const heading = React.createElement(
   "h1",
