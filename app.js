dc21097f440e932cf8b0bf7d3950cc50f431f703
Manu FG <manufg1904@gmail.com>
2024-11-28 09:47:22 +0530

    Using JSX.

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-/* 
-  <div id ="root">
-   <div id =" parent">
-    <div id = "child"
-     <h1 id ="heading"> nested structure </h1>
-     </div>
-     </div>
-     </div>
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 
-const nested = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading" }, "h1 tag is here"),
-    React.createElement("h2", { id: "heading1" }, "h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "h1 tag"),
-    React.createElement("h2", { id: "heading1" }, "h2 tag"),
-  ]),
-]);
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React"); // this is not a html element it is an object
 
-// this is vry tideous thats why JSX exists.
+//JSX-HTML like or XML like syntax
+const jsxHeading = <h1 id="heading"> Using JSX </h1>;
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
-
-console.log(heading); // this is a js object. React.create Element is basically returning an object.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nested);
+root.render(jsxHeading);
