957735c947d3168d471324430b738ed943c9322e
Manu FG <manufg1904@gmail.com>
2025-02-13 00:50:25 +0530

    using jsx

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom";
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React"); // this is not a html element it is an object
 
 //JSX-HTML like or XML like syntax
-const jsxHeading = <h1 id="heading"> Using JSX </h1>;
+const jsxHeading = (
+  <h1 id="heading" className="Manu">
+    {" "}
+    Using JSX{" "}
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
