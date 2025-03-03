8d9ca9a841f78aaaa906043a82aee0abcac5e63b
Manu FG <manufg1904@gmail.com>
2025-03-03 22:41:38 +0530

    comment added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//React.createElement -> React Element Js object -> HTMLElement(render)
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React"); // this is not a html element it is an object
 
-//JSX-HTML like or XML like syntax
+//JSX-HTML like or XML like syntax ( jsx is transpiled before it reaches the JS parcel -> Babel)
+//JSX-> React.Create Element -> React Element Js object -> HTMLElement(render)
 const jsxHeading = (
   <h1 id="heading" className="Manu">
     {" "}
