fbc01057b5a37623952422d4a80ff6e8c06a9c47
Manu FG <manufg1904@gmail.com>
2025-03-04 09:30:38 +0530

    Rendering components

--- app.js
+++ app.js
@@ -8,10 +8,28 @@ const heading = React.createElement("h1", { id: "heading" }, "Namaste React"); /
 //JSX-> React.Create Element -> React Element Js object -> HTMLElement(render)
 const jsxHeading = (
   <h1 id="heading" className="Manu">
-    {" "}
-    Using JSX{" "}
+    Using JSX
   </h1>
 );
 
+//React components
+//Class based components - OLD
+//Functional components - NEW
+
+//React functional component
+
+const HeadingComponent0 = () => {
+  //same  but this is beeter syntax with return
+  return <h1> Namaste Manu </h1>;
+};
+
+const HeadingComponent1 = () => (
+  //same
+  //COMPONENT
+  <h1 className="heading"> Namste Manu </h1>
+);
+
+const HeadingComponent2 = () => <h1> Namste Manu </h1>; //Both syntax are for the same thing
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent0 />);
