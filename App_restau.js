dfeaeeeb6ec024ae66335242401c85a2ab3b366e
Manu FG <manufg1904@gmail.com>
2025-05-18 12:53:11 +0530

    create new app restaurant file .

---
+++ App_restau.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+/**
+ * 
+ * Header 
+ *   - Logo
+ *  - Nav Items
+ * * Body
+ *   - Search bar
+ *   - Restaurant Container
+ * *     - Restaurant Card
+ * Footer
+ *    -Copyright
+ *   - Links
+ * *   - Address
+ * *   - Contact
+ * *   - Social Media Links
+ */
+
+
+ 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent1/>);
